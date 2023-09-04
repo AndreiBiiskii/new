f523b4183ceb07a3ecd3373cbab7ba931bc87966
andrei <freemail_2019@mail.ru>
2023-09-04 19:37:56 +0800

    change SELECT

--- SELECT.sql
+++ SELECT.sql
@@ -16,12 +16,51 @@ WHERE POSITION (' ' IN s.name) = 0;
 
 -- Название треков, которые содержат слово «мой» или «my».
 SELECT title FROM track t 
-WHERE upper(title) LIKE '%MY%' OR upper(title) LIKE '%МОЙ%' 
+WHERE upper(title) LIKE '%MY%' OR upper(title) LIKE '%МОЙ%';
 
 --Количество исполнителей в каждом жанре.
 SELECT g.name, count(*) FROM singergenre s
 LEFT JOIN genre g ON g.genre_id = s.genre_id 
-GROUP BY g.name  
+GROUP BY g.name ; 
+
+-- Количество треков, вошедших в альбомы 2019–2020 годов.
+SELECT a.date_create, count(*) FROM track t 
+LEFT JOIN album a ON a.album_id = t.album_id 
+WHERE a.date_create BETWEEN '2019-01-01' AND '2020-12-31'
+GROUP BY a.date_create;
+
+-- Средняя продолжительность треков по каждому альбому.
+SELECT a.album_id , count(*) FROM track t 
+LEFT JOIN album a ON a.album_id = t.album_id 
+GROUP BY a.album_id;
+ 
+-- Средняя продолжительность треков по каждому альбому.
+SELECT a.album_id , avg(duration) FROM track t 
+LEFT JOIN album a ON a.album_id = t.album_id 
+GROUP BY a.album_id;
+
+-- Все исполнители, которые не выпустили альбомы в 2020 году.
+SELECT name FROM singer s 
+WHERE name NOT IN 
+    (SELECT s2.name FROM singer s2 
+    LEFT JOIN singeralbum s3 ON s2.singer_id = s3.singer_id
+    LEFT JOIN album a ON a.album_id = s3.album_id
+    WHERE a.date_create BETWEEN '2020-01-01' AND '2020-12-31');
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
