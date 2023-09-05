a6e3c39cd882d5b4438b0ad3996f717b9b834430
andrei <freemail_2019@mail.ru>
2023-09-06 00:49:10 +0800

    добавил алиасы в SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -1,41 +1,41 @@
 -- Название и продолжительность самого длительного трека.
-SELECT title, duration  FROM track t 
+SELECT title трек, duration продолжительность  FROM track t 
 WHERE duration = (SELECT max(duration) FROM track t2 );
 
 -- Название треков, продолжительность которых не менее 3,5 минут.
-SELECT title FROM track t 
+SELECT title трек FROM track t 
 WHERE duration / 60 > 3.5;
 
 -- Названия сборников, вышедших в период с 2018 по 2020 год включительно.
-SELECT name FROM compilation c 
+SELECT name сборник FROM compilation c 
 WHERE date_create BETWEEN '2018-01-01' AND '2020-12-31';
 
 -- Исполнители, чьё имя состоит из одного слова.
-SELECT name FROM singer s 
+SELECT name исполнитель FROM singer s 
 WHERE POSITION (' ' IN s.name) = 0;
 
--- Название треков, которые содержат слово «мой» или «my».
-SELECT title FROM track t 
+-- Название треков, которые содержат слово «мой» или «me».
+SELECT title трек FROM track t 
 WHERE upper(title) LIKE '%ME%' OR upper(title) LIKE '%МОЙ%';
 
 --Количество исполнителей в каждом жанре.
-SELECT g.name, count(*) FROM singergenre s
+SELECT g.name жанр, count(*) количество_исполнителей FROM singergenre s
 LEFT JOIN genre g ON g.genre_id = s.genre_id 
 GROUP BY g.name ; 
 
 -- Количество треков, вошедших в альбомы 2019–2020 годов.
-SELECT a.date_create, count(*) FROM track t 
+SELECT a.date_create дата_выпуска, count(*) количество FROM track t 
 LEFT JOIN album a ON a.album_id = t.album_id 
 WHERE a.date_create BETWEEN '2019-01-01' AND '2020-12-31'
 GROUP BY a.date_create;
  
 -- Средняя продолжительность треков по каждому альбому.
-SELECT a.album_id , avg(duration) FROM track t 
+SELECT a.album_id альбом, avg(duration) средняя_продолжительность FROM track t 
 LEFT JOIN album a ON a.album_id = t.album_id 
 GROUP BY a.album_id;
 
 -- Все исполнители, которые не выпустили альбомы в 2020 году.
-SELECT name FROM singer s 
+SELECT name исполнители FROM singer s 
 WHERE name NOT IN 
     (SELECT s2.name FROM singer s2 
     LEFT JOIN singeralbum s3 ON s2.singer_id = s3.singer_id
@@ -44,7 +44,7 @@ WHERE name NOT IN
     
 -- Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами)
 -- Есть подозрения, что этот подход не очень, но вроде работает, вручную подсчитал))
-SELECT DISTINCT   s2.name, c.name FROM compilation c 
+SELECT DISTINCT s2.name исполнитель, c.name сборник FROM compilation c 
 LEFT JOIN trackcompilation t ON t.compilation_id = c.compilation_id 
 LEFT JOIN track t2 ON t2.track_id = t.track_id
 LEFT JOIN album a ON a.album_id = t2.album_id 
@@ -53,16 +53,16 @@ LEFT JOIN singer s2 ON s2.singer_id = s.singer_id
 WHERE s2.name = 'Rihanna';
 
 -- Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
-SELECT DISTINCT a.name FROM album a 
+SELECT DISTINCT a.name альбом FROM album a 
 LEFT JOIN singeralbum s2 ON a.album_id = s2.album_id 
 LEFT JOIN singer s3 ON s2.singer_id = s3.singer_id 
 WHERE s3.singer_id IN 
 (SELECT  s.singer_id   FROM singergenre s 
 GROUP BY s.singer_id, s3.name 
-having  count(s.genre_id) > 3); 
+having  count(s.genre_id) > 1); 
 
 -- Наименования треков, которые не входят в сборники
-SELECT t.title  FROM track t
+SELECT t.title трек  FROM track t
 LEFT JOIN trackcompilation t2 ON t2.track_id = t.track_id
 WHERE  t2.track_id IS NULL ;
 
@@ -75,7 +75,7 @@ WHERE t.duration =
 (SELECT min(duration) FROM track);
 
 -- Названия альбомов, содержащих наименьшее количество треков
-SELECT a.name , t.album_id , count(*) c FROM track t
+SELECT a.name альбом FROM track t
 LEFT JOIN album a ON t.album_id = a.album_id 
 GROUP BY a.name, t.album_id 
 HAVING count(*) = (SELECT min(t.c) FROM 
